549585ce05090a0d752a1c3e35320dc31c23ddd7
Lucas Gutheim <lucas.gutheim@gmail.com>
2024-09-25 18:43:33 -0700

    Holy shit it works

--- enigma.c
+++ enigma.c
@@ -3,29 +3,33 @@
 #include "enigma.h"
 #include "rotors.h"
 
-// A char was missing from each of the rotors but I added them back randomly
-// Made sure to make the arrays size 26 so there is no null terminator
-const char ROTOR_1[26] = "EKMFGHDQVZXRUTIABJSYCLWPON";
-const char ROTOR_2[26] = "AJDKSIRUBLHXQTMOWZYVFCPNGE";
-const char ROTOR_3[26] = "BDFHJLNPRTVKXZMOQSUWYACEGI";
-const char ROTOR_4[26] = "";
-const char ROTOR_5[26] = "";
+const char ROTOR_1[27] = "EKMFGHDQVZXRUTIABJSYCLWPON";
+const char ROTOR_2[27] = "AJDKSIRUBLHXQTMOWZYVFCPNGE";
+const char ROTOR_3[27] = "BDFHJLNPRTVKXZMOQSUWYACEGI";
+const char ROTOR_4[27] = "KCPXSYOMZRTLEVFWUNAIQGHBDJ";
+const char ROTOR_5[27] = "LZIJTBYNREOAPUVDWHXCKQMGFS";
 
-const char REFLECTOR_1[26] = "COAHIJRDEFQZWYBUKGXVPTMSNL";
-const char REFLECTOR_2[26] = "WKPZGHEFVQBXRYUCJMTSOIALND";
+const char REFLECTOR_1[27] = "COAHIJRDEFQZWYBUKGXVPTMSNL";
+const char REFLECTOR_2[27] = "WKPZGHEFVQBXRYUCJMTSOIALND";
 
 void assignRotor(char *r, int rNum) {
+
   switch (rNum) {
   case 1:
-    strncpy(r, ROTOR_1, 26);
+    strncpy(r, ROTOR_1, 27);
+    break;
   case 2:
-    strncpy(r, ROTOR_2, 26);
+    strncpy(r, ROTOR_2, 27);
+    break;
   case 3:
-    strncpy(r, ROTOR_3, 26);
+    strncpy(r, ROTOR_3, 27);
+    break;
   case 4:
-    strncpy(r, ROTOR_4, 26);
+    strncpy(r, ROTOR_4, 27);
+    break;
   case 5:
-    strncpy(r, ROTOR_5, 26);
+    strncpy(r, ROTOR_5, 27);
+    break;
   }
 }
 
@@ -45,7 +49,7 @@ char isValidPlugboard(char *arr) {
 char isNumber(char *arr) {
 
   for (int i = 0; i < strlen(arr); ++i) {
-    if (arr[i] > '9' || arr[i] < '1') {
+    if (arr[i] > '9' || arr[i] < '0') {
       return 1;
     }
   }
@@ -53,10 +57,17 @@ char isNumber(char *arr) {
   return 0;
 }
 
+void printCypher(const char *c) {
+  for (int i = 0; i < 26; ++i) {
+    printf("%c", c[i] + 'A');
+  }
+  printf("\n");
+}
+
 int main(int argc, char *argv[]) {
 
   if (argc < 2) {
-    fprintf(stderr, "Invalid argument count");
+    fprintf(stderr, "Invalid argument count\n");
     return 1;
   }
 
@@ -64,42 +75,44 @@ int main(int argc, char *argv[]) {
 
   char *string;
 
-  char r1[26];
-  char r2[26];
-  char r3[26];
-  char rfl[26];
+  // 27 so there is a space for a null terminator
+  char r1[27];
+  char r2[27];
+  char r3[27];
+  char rfl[27];
 
   // copy in default cyphers
-  strncpy(r1, ROTOR_1, 26);
-  strncpy(r2, ROTOR_2, 26);
-  strncpy(r3, ROTOR_3, 26);
-  strncpy(rfl, REFLECTOR_1, 26);
-  // not copying the null terminator is a little dangerous but I think I handled
-  // it
+  strncpy(r1, ROTOR_1, 27);
+  strncpy(r2, ROTOR_2, 27);
+  strncpy(r3, ROTOR_3, 27);
+  strncpy(rfl, REFLECTOR_1, 27);
 
   // Random Default Values
   int r1pos = 0;
   int r2pos = 1;
   int r3pos = 2;
 
-  char plugboard[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  char plugboard[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   int notch1 = 5;
   int notch2 = 10;
 
   int *ptr; // temp variable used for switch cases
 
+  // flags
+  char verbose = 0;
+
   for (int i = 1; i < argc; ++i) {
     if (argv[i][0] == '-') {
       switch (argv[i][1]) {
       case 'b':
         if (strlen(argv[i + 1]) != 26) {
-          fprintf(stderr, "Invalid Pugboard Cypher Length '%lu' ",
+          fprintf(stderr, "Invalid Pugboard Cypher Length '%lu'\n",
                   strlen(argv[i + 1]));
           return 2;
         }
         if (isValidPlugboard(argv[i + 1]) != 0) {
-          fprintf(stderr, "Pluboard Cypher is Invalid");
+          fprintf(stderr, "Pluboard Cypher is Invalid\n");
           return 2;
         }
 
@@ -109,7 +122,7 @@ int main(int argc, char *argv[]) {
       case 'r':
         for (int j = 0; j < 3; ++j) {
           if (argv[i + 1][j] > '5' || argv[i + 1][j] < '1') {
-            fprintf(stderr, "Invalid Rotor Number '%c'", argv[i + 1][j]);
+            fprintf(stderr, "Invalid Rotor Number '%c'\n", argv[i + 1][j]);
             return 3;
           }
         }
@@ -130,7 +143,7 @@ int main(int argc, char *argv[]) {
           strncpy(rfl, REFLECTOR_2, 26);
           break;
         default:
-          fprintf(stderr, "Invalid Reflector Number '%c'", argv[i + 1][0]);
+          fprintf(stderr, "Invalid Reflector Number '%c'\n", argv[i + 1][0]);
           return 4;
         }
 
@@ -148,22 +161,22 @@ int main(int argc, char *argv[]) {
           ptr = &r3pos;
           break;
         default:
-          fprintf(stderr, "Invalid Rotor Number '%s'", argv[i]);
+          fprintf(stderr, "Invalid Rotor Number '%s'\n", argv[i]);
           return 5;
         }
 
         if (isNumber(argv[i + 1]) != 0) {
-          fprintf(stderr, "Invalid Rotor Position '%s'", argv[i + 1]);
+          fprintf(stderr, "Invalid Rotor Position Format '%s'\n", argv[i + 1]);
           return 5;
         }
 
         *ptr = atoi(argv[i + 1]);
 
         if (*ptr > 25 || *ptr < 0) {
-          fprintf(stderr, "Invalid Rotor Position '%d'", *ptr);
+          fprintf(stderr, "Invalid Rotor Position '%d'\n", *ptr);
           return 5;
         }
-
+        break;
       case 'n':
 
         switch (argv[i][2]) {
@@ -174,40 +187,43 @@ int main(int argc, char *argv[]) {
           ptr = &notch2;
           break;
         default:
-          fprintf(stderr, "Invalid Notch Number '%s'", argv[i]);
+          fprintf(stderr, "Invalid Notch Number '%s'\n", argv[i]);
           return 6;
         }
 
         if (isNumber(argv[i + 1]) != 0) {
-          fprintf(stderr, "Invalid Notch Position '%s'", argv[i + 1]);
+          fprintf(stderr, "Invalid Notch Position '%s'\n", argv[i + 1]);
           return 6;
         }
 
         *ptr = atoi(argv[i + 1]);
 
         if (*ptr > 25 || *ptr < 0) {
-          fprintf(stderr, "Invalid Notch Position '%d'", *ptr);
+          fprintf(stderr, "Invalid Notch Position '%d'\n", *ptr);
           return 6;
         }
 
         i++;
         break;
+      case 'v':
+        verbose = 1;
+        break;
       case 'f':
-        fprintf(stderr, "File input is not yet supported");
+        fprintf(stderr, "File input is not yet supported\n");
         return 10;
 
         break;
       case '-':
-        fprintf(stderr, "Config files are not yet supported");
+        fprintf(stderr, "Config files are not yet supported\n");
         return 10;
         break;
       case 'h':
 
-        printf("Sorry I haven't made it yet :(");
+        printf("Sorry I haven't made it yet :(\n");
         return 0;
         break;
       default:
-        printf("Invalid flag '%s'\nUse 'enigma -h' for useage", argv[i]);
+        printf("Invalid flag '%s'\nUse 'enigma -h' for useage\n", argv[i]);
         break;
       }
     } else // the only other case is that it is the input string
@@ -223,26 +239,39 @@ int main(int argc, char *argv[]) {
     r2[i] -= 'A';
     r3[i] -= 'A';
     rfl[i] -= 'A';
+    plugboard[i] -= 'A';
   }
 
-  if (r1pos != 0)
-  {
+  if (r1pos != 0) {
     rotate(r1, r1pos, 26);
   }
-  if (r2pos != 0)
-  {
+  if (r2pos != 0) {
     rotate(r2, r2pos, 26);
   }
-  if (r3pos != 0)
-  {
+  if (r3pos != 0) {
     rotate(r3, r3pos, 26);
+  }
+
+  if (verbose != 0) {
+    printf("String: %s\n", string);
+    printf("r1 - %d: ", r1pos);
+    printCypher(r1);
+    printf("r2 - %d: ", r2pos);
+    printCypher(r2);
+    printf("r3 - %d: ", r3pos);
+    printCypher(r3);
+    printf("rfl: ");
+    printCypher(rfl);
+    printf("PB: ");
+    printCypher(plugboard);
+    printf("n1: %d, n2: %d\n", notch1, notch2);
   }
 
   char *output;
   output = Enigma(string, r1, r2, r3, rfl, r1pos, r2pos, r3pos, plugboard,
                   notch1, notch2);
 
-  printf("%s", output);
+  printf("%s\n", output);
 
   free(string);
   free(output);
--- enigma.h
+++ enigma.h
@@ -42,9 +42,10 @@ void spinRotors(char *r1, char *r2, char *r3, int *r1count, int *r2count,
   rotate(r1, 1, 26);
 }
 
-char *Enigma(char *string, char r1[26], char r2[26], char r3[26], char rfl[26], int r1pos,
-             int r2pos, int r3pos, const char plugboard[26], const int notch1,
-             const int notch2) {
+char *Enigma(char *string, char r1[27], char r2[27], char r3[27], char rfl[27],
+             int r1pos, int r2pos, int r3pos, const char plugboard[27],
+             const int notch1, const int notch2) {
+
 
   char *output = (char *)malloc(strlen(string) + 1);
 
@@ -52,9 +53,8 @@ char *Enigma(char *string, char r1[26], char r2[26], char r3[26], char rfl[26], 
 
     char letter = string[i];
     char index = alphabetIndex(letter);
-    
-    if (index == -1)
-    {
+
+    if (index == -1) {
       // Skip all non-alphabetical characters
       output[i] = letter;
       continue;
